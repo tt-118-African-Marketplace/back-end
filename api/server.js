bb33c908691fe187bb4a4e691d1ff90cad0d4661
Ian-Tucker <iandecisiv@gmail.com>
2021-03-24 19:27:13 -0500

    serverjs

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+
+
+const configureRoutes = require("../config/routes.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+configureRoutes(server);
+
+server.get("/", (req, res) => {
+    res.send("You are now in the African Marketplace");
+});
+
+module.exports = server;
